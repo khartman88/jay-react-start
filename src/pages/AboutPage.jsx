1bd35ffae960f234001941f685fcee701662a45e
Kyle Hartman <khartman@nhcgov.com>
2025-06-08 22:37:16 -0400

    pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+
+// about page //
+function AboutPage() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>This is a todo list app built with React Router and Airtable.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
